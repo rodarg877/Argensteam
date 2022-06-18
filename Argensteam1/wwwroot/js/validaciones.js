676f80872536037edf770ac7bb053b100257896f
Adriel <adrielgentil@gmail.com>
2022-06-18 16:56:07 -0300

    Validaciones de nombre de usuario y contrasnia

---
+++ Argensteam1/wwwroot/js/validaciones.js
@@ -0,0 +1,31 @@
+﻿using String.prototype.match;
+
+function validarNombre(nombreUsuario) {
+    var pudo = false;
+    if (nombreUsuario.length <= 6 && nombreUsuario.length >= 30) {
+        document.getElementById("ususarioInv").style.visibility = "visible";
+    } else {
+        document.getElementById("ususarioInv").style.visibility = "hidden";
+        pudo = true;
+    }
+}
+
+function validarContrasenia(contrasenia) {
+    var pudo = false;
+    if (contrasenia <= 8 && !contrasenia.match(/\d/)) {
+        document.getElementById("contraseniaInv").style.visibility = "visible";
+    } else {
+        document.getElementById("contraseniaInv").style.visibility = "hidden";
+        pudo = true;
+    }
+}
+
+function validarFormulario(event) {
+    var nombreVal = validarNombre(document.getElementById("contrasenia"));
+    var contrVal = validarContrasenia(document.getElementById("ususario"));
+
+    if (!nombreVal || !contrVal) {
+        event.preventDefault();
+    }
+
+}
